2b90507dc0e6c957195cb23169827f9ff982659f
staxx19 <isaiahbrant481@gmail.com>
2025-04-29 09:01:11 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Inheritance_brant_isaiah
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                FPS myFPS = new FPS("E", "ZombCube");
+
+                Console.WriteLine(myFPS.Describe());
+                myFPS.PlayGame();
+
+                FPS csGo = new FPS("M", "CS:GO");
+                Console.WriteLine(csGo.Describe());
+                csGo.PlayGame();
+
+            }
+        }
+    }
+}
